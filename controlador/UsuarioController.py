e55529aa3702ae2d053a47cbdeefd97ff6597e52
LFP2002 <fuentesparralucas@gmail.com>
2025-11-21 17:29:30 -0300

    Revert "Autenticar seguro"
    This reverts commit 4c61a45a63710fee5bec96fc41d96749c00690e8.

--- controlador/UsuarioController.py
+++ controlador/UsuarioController.py
@@ -9,32 +9,12 @@ class UsurarioController:
 
     def nuevo_usuario(self, nombre, apellidos, nickname, email, telefono, rol_id, contrasena):
         if not nombre or not apellidos or not nickname or not email or not telefono or not rol_id or not contrasena:
-            print("Todos los campos son obligatorios.")
-            return
+            raise ValueError("Todos los campos son obligatorios.")
         elif not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            print("El formato del correo electrónico es inválido.")
-            return
-        elif not re.match(r"^\+?\d{8,9}$", telefono):
-            print("El formato del número de teléfono es inválido.")
-            return
+            raise ValueError("El formato del correo electrónico es inválido.")
+        elif not re.match(r"^\+?\d{10,15}$", telefono):
+            raise ValueError("El formato del número de teléfono es inválido.")
         elif len(contrasena) < 8:
-            print("La contraseña debe tener al menos 8 caracteres.")
-            return
-        elif rol_id != 1 and rol_id != 2:
-            print("El ID de rol debe ser 1 (Usuario) o 2 (Administrador).")
-            return
+            raise ValueError("La contraseña debe tener al menos 8 caracteres.")
         else:
             self.__usuario.registrar_usuario(nombre, apellidos, nickname, email, telefono, rol_id, contrasena)
-
-    def autenticar_usuario(self, nickname, contrasena):
-        if not nickname or not contrasena:
-            print("El nombre de usuario y la contraseña son obligatorios.")
-            return None
-        else: 
-            sesion = self.__usuario.autenticar_usuario(nickname, contrasena)
-            if sesion:
-                print("Autenticación exitosa.")
-                return sesion
-            else:
-                print("Nombre de usuario o contraseña incorrectos.")
-                return None
